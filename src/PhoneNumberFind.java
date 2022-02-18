623d519b3700df919b6e93cf430ef2ca8f955a0e
LevytskyiYurii <levytskyi1@gmail.com>
2022-02-18 18:54:06 +0200

    Task_1

--- src/PhoneNumberFind.java
+++ src/PhoneNumberFind.java
@@ -1,12 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PhoneNumberFind {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\file.txt");
         Scanner scanner = new Scanner(file);
-        System.out.println(scanner.nextLine());
 
+        while (scanner.hasNext()){
+            List<String> numbers = Collections.singletonList(scanner.nextLine());
+            Stream<String> numbersStream = numbers.stream()
+                            .filter((number) -> number.contains("-"));
+            List<String> phoneNumbers = numbersStream.collect(Collectors.toList());
+
+            System.out.println(phoneNumbers);
+        }
     }
 }
