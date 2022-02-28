89fcf720a902aeca9cc2436d3cc710061db95b76
LevytskyiYurii <levytskyi1@gmail.com>
2022-02-28 11:30:17 +0200

    Task_2

--- src/main/java/Task_1/PhoneNumberFind.java
+++ src/main/java/Task_1/PhoneNumberFind.java
@@ -6,16 +6,24 @@ import java.util.Scanner;
 
 public class PhoneNumberFind {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\file.txt");
+        File file = new File("C:\\file.txt");//(123)-456-7890
         Scanner scanner = new Scanner(file);
 
-        while (scanner.hasNext()){
-            List<String> numbers = Collections.singletonList(scanner.nextLine());
-            Stream<String> numbersStream = numbers.stream()
-                            .filter((number) -> number.contains("-"));
-            List<String> phoneNumbers = numbersStream.collect(Collectors.toList());
+        while (scanner.hasNext()) {
+//            List<String> numbers = Collections.singletonList(scanner.nextLine());
+//            List<String> phoneNumbers = numbers.stream()
+//                    .filter((number) -> number.contains("-"))
+//                    .collect(Collectors.toList());
+//
+//            phoneNumbers.removeIf(String::isBlank);
+//
+//            System.out.println(phoneNumbers);
+            String number = scanner.nextLine();
+            if (number.contains("-") && !number.isBlank()) System.out.println(number);
 
-            System.out.println(phoneNumbers);
         }
     }
 }
+//[987-123-4567]
+//        []
+//        [(123) 456-7890]
